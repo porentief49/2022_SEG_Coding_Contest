85cbd2dd58211b9e02320fff4c7686253a782d95
grr <rainer.gruber@mindscale.com>
2022-01-14 11:49:37 +0100

    continue rename

--- AoC_vb/My Project/Settings.Designer.vb
+++ AoC_vb/My Project/Settings.Designer.vb
@@ -64,9 +64,9 @@ Namespace My
     Friend Module MySettingsProperty
         
         <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>  _
-        Friend ReadOnly Property Settings() As Global.AoC_vb.My.MySettings
+        Friend ReadOnly Property Settings() As Global.SegCC_vb.My.MySettings
             Get
-                Return Global.AoC_vb.My.MySettings.Default
+                Return Global.SegCC_vb.My.MySettings.Default
             End Get
         End Property
     End Module
